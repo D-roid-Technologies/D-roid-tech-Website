fffc1f5a764f33804ecb12cb895cbf71a500455d
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-04-20 10:29:54 +0100

    moved course data

--- src/app/utils/constant/Data.ts
+++ src/app/utils/constant/Data.ts
@@ -44,4 +44,90 @@ export const DATA = {
       path: "/staff",
     },
   ],
+  courses:[
+    {
+      id: 1,
+      title: "Front-End Development (React)",
+      subtitle: "Master Front-End Development with React.",
+      description:
+        "Welcome to the world of Frontend Development with React Js, where you'll learn to create stunning and dynamic user interfaces for web applications. React Js is a powerful JavaScript library that enables you to build reusable UI components, making the development process efficient and enjoyable.",
+      benefits: [
+        {
+          title: "Html & css",
+          description:
+            "Master the foundational languages of the web – HTML and CSS. Learn how to structure web pages using HTML, style them using CSS, and create visually appealing layouts and designs.",
+        },
+        {
+          title: "Javascript",
+          description:
+            "Deepen your understanding of Javascript, the programming language that powers interactivity on the web. Learn essential JavaScript concepts and techniques to enhance the functionality of your React applications.",
+        },
+        {
+          title: "React Fundamentals",
+          description:
+            "Understand the core principles of React, including component-based architecture, JSX syntax, and virtual DOM. Learn how to build reusable UI components and effectively manage state in React applications.",
+        },
+        {
+          title: "State Management with Redux",
+          description:
+            "Dive into Redux, a powerful state management library for React applications. Learn how to centralize and manage application state, handle complex data flows, and implement actions and reducers for predictable state updates.",
+        },
+        {
+          title: "React Router",
+          description:
+            "Explore react router, a popular routing library for building single-page application. Learn how to implement navigation, handle routing, and create dynamic routes to create a smooth and seamless user experience.",
+        },
+        {
+          title: "API integration",
+          description:
+            "Learn how to integrate external APIs into your react applications to fetch and display dynamic data. Understand concepts like asynchronous programming, handling API responses and managing data flow between components.",
+        },
+      ],
+      projects: [
+        {
+          title: "Calculator App",
+          description:
+            "Get ready to crunch numbers with style! In just two weeks, you'll build your very own Calculator App using Flutter",
+        },
+        {
+          title: "Event App",
+          description:
+            "It's time to unleash your inner event planner! In just five weeks, you'll create an Event App that takes event management to the next level.",
+        },
+        {
+          title: "Fintech App",
+          description:
+            "Get ready to dive into the world of finance and technology! As the final project of the course, you'll develop a dynamic and secure Fintech App that empowers users to manage their finances with ease.",
+        },
+      ],
+      howItWorks: [
+        {
+          title: "Register",
+          content:
+            "Sign up and select the tech skill you want to learn. The system matches you with a group of three to four other users who are also interested in learning the same skill",
+        },
+        {
+          title: "Assign",
+          content:
+            "The group is assigned to dedicated mentor who is an expert in the chosen skill. The mentor leads the group through a structured learning program that includes video lectures, interactive exercises and quizzes.",
+        },
+        {
+          title: "Interact",
+          content:
+            "The group meets weekly via video conference to discuss their progress, ask questions, and receive feedback from the mentor and each other.",
+        },
+        {
+          title: "Get Certified",
+          content: "Upon completion of the program, users receive a certificate of achievement.",
+        },
+      ],
+      courseDetails: {
+        startDate: "1st August, 2023",
+        duration: "10 weeks",
+        price: 250000,
+        discountedPrice: 60000,
+        offerExpiry: "21 : 10 : 05",
+      },
+    },
+  ]
 };
