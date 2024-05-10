d639cc2e107d948b8be09ac6cf83e8de5d14b25a
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-05-10 16:40:10 +0100

    new nav link

--- src/app/utils/constant/Data.ts
+++ src/app/utils/constant/Data.ts
@@ -11,7 +11,7 @@ export const DATA = {
     },
     {
       link: "Software Development",
-      path: "/programminghome",
+      path: "/software",
     },
     {
       link: "Animation Creation",
