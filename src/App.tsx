3e2f5f00a2bbbc214485b5d697b3a739eb55f687
developerekene <developerekene@gmail.com>
2024-06-04 18:20:16 +0100

    adding to production

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AppEntry from "./app/ui/AppEntry";
 import { store } from "./app/redux/Store";
 
 function App() {
+  // The App.js file
   return (
     <Provider store={store}>
       <AppEntry />
