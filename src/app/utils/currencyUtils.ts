90bf15551ac905e562b11e52fdf6dccad35ebd42
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2024-06-05 17:29:26 +0100

    rewrote and optimized the currency function

--- src/app/utils/currencyUtils.ts
+++ src/app/utils/currencyUtils.ts
@@ -1,39 +1,66 @@
 import axios from "axios";
-import { getUserLocation } from "./locationUtils";
-
-const API_KEY= process.env.CURRENCY_API_KEY;
 
 export const getLocalCurrency = async (): Promise<string> => {
   try {
-    const { latitude, longitude } = await getUserLocation();
-    const response = await axios.get(
-      `https://api.exchangerate-api.com/v4/latest/USD?apiKey=${API_KEY}&base=USD&format=json`
-    );
+    const response = await axios.get("https://ipapi.co/currency/");
+    console.log("currency", response.data);
+    const localCurrencyCode = response.data;
 
-    const localCurrencyCode = "GBP";
-
-    return localCurrencyCode;
+    if (localCurrencyCode) {
+      return localCurrencyCode;
+    } else {
+      const defaultCurrencyCode = "USD";
+      console.warn(
+        "Failed to fetch local currency from ipapi.co. Using default currency code:",
+        defaultCurrencyCode
+      );
+      return defaultCurrencyCode;
+    }
   } catch (error) {
     console.error("Error fetching local currency:", error);
-    return "USD";
+    const defaultCurrencyCode = "USD";
+    console.warn(
+      "Error fetching local currency. Using default currency code:",
+      defaultCurrencyCode
+    );
+    return defaultCurrencyCode;
   }
 };
 
-export const convertToCurrency = async (
-  amount: number,
-  targetCurrency: string
-): Promise<string> => {
+export const convertToCurrency = async (amount: number): Promise<string> => {
   try {
+    const targetCurrency = await getLocalCurrency();
     const response = await axios.get(
-      `https://api.exchangerate-api.com/v4/latest/USD?apiKey=${API_KEY}&base=USD&symbols=${targetCurrency}&format=json`
+      `https://api.exchangerate-api.com/v4/latest/GBP`
     );
 
     const exchangeRate = response.data.rates[targetCurrency];
+    if (!exchangeRate) {
+      throw new Error(`Exchange rate for ${targetCurrency} not found`);
+    }
+
     const convertedAmount = amount * exchangeRate;
 
-    return convertedAmount.toFixed(2);
+    // Format the result using Intl.NumberFormat
+    const formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: targetCurrency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    return formatter.format(convertedAmount);
   } catch (error) {
     console.error("Error converting currency:", error);
-    return amount.toFixed(2);
+
+    // Fallback to formatting the amount in GBP if conversion fails
+    const formatter = new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency: "GBP",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    return formatter.format(amount);
   }
 };
