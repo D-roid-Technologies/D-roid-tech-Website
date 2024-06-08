4a51ddd79df3de47b93f68335812d5f8bbdf0cab
developerekene <developerekene@gmail.com>
2024-06-08 16:38:50 +0100

    updating the about us page

--- src/app/ui/pages/aboutus/AboutUs.tsx
+++ src/app/ui/pages/aboutus/AboutUs.tsx
@@ -114,7 +114,7 @@ const AboutUs: React.FunctionComponent = () => {
                     At D'roid Technologies, integrity is the cornerstone of our
                     operations. We are committed to conducting our business with
                     the highest ethical standards, ensuring transparency,
-                    honesty, and accountability in all our interactions.
+                    honesty, and accountability in all our interactions. This is new.
                   </p>
                 </div>
                 <div>
