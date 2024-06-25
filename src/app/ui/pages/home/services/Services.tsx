96e985295c0b0f23faadc97cccd4cf59c485b3c0
developerekene <developerekene@gmail.com>
2024-06-25 19:29:36 +0100

    reverted to the formal project

--- src/app/ui/pages/home/services/Services.tsx
+++ src/app/ui/pages/home/services/Services.tsx
@@ -24,8 +24,7 @@ type Service = {
   title: string;
   description: string;
   icon: React.ReactNode;
-  // prices: { name: string; price: number }[];
-  button: string;
+  prices: { name: string; price: number }[];
 };
 
 const services: Service[] = [
@@ -34,61 +33,56 @@ const services: Service[] = [
     description:
       "Our software development services cover both mobile and web applications. We specialize in creating responsive, high-performance applications using modern technologies such as React Native, React.js, HTML, CSS, TypeScript, and JavaScript. Whether you need a custom web application or a cross-platform mobile app, we deliver solutions that meet your business requirements.",
     icon: <FaLaptopCode size={48} color={Assets.colors.primary} />,
-    button: require("../../programming/ProgrammingHome"),
-    // prices: [
-    //   { name: "Basic Web Application", price: 1000 },
-    //   { name: "Complex Web Application", price: 5000 },
-    //   { name: "Simple Mobile App", price: 3000 },
-    //   { name: "Advanced Mobile App", price: 15000 },
-    // ],
+    prices: [
+      { name: "Basic Web Application", price: 1000 },
+      { name: "Complex Web Application", price: 5000 },
+      { name: "Simple Mobile App", price: 3000 },
+      { name: "Advanced Mobile App", price: 15000 },
+    ],
   },
   {
     title: "Animation Creation",
     description:
       "Our animation creation services bring your ideas to life with stunning visuals. We produce high-quality animations for various purposes, including marketing, educational content, and entertainment. Our team works closely with you to understand your vision and deliver animations that captivate your audience.",
     icon: <FaVideo size={48} color={Assets.colors.primary} />,
-    button: "",
-    // prices: [
-    //   { name: "Short Animation (up to 1 minute)", price: 1000 },
-    //   { name: "Medium Animation (1-3 minutes)", price: 3000 },
-    //   { name: "Long Animation (over 3 minutes)", price: 7000 },
-    // ],
+    prices: [
+      { name: "Short Animation (up to 1 minute)", price: 1000 },
+      { name: "Medium Animation (1-3 minutes)", price: 3000 },
+      { name: "Long Animation (over 3 minutes)", price: 7000 },
+    ],
   },
   {
     title: "Tech Trainings",
     description:
       "We offer comprehensive tech training programs designed to enhance your team's skills and keep them up-to-date with the latest technologies. Our training sessions cover various topics such as software development, cybersecurity, data analytics, and more, delivered by industry experts.",
     icon: <FaRobot size={48} color={Assets.colors.primary} />,
-    button: require("../../training/Training"),
-    // prices: [
-    //   { name: "One-Day Workshop", price: 500 },
-    //   { name: "Week-Long Course", price: 2000 },
-    //   { name: "Customized Training Program", price: 5000 },
-    // ],
+    prices: [
+      { name: "One-Day Workshop", price: 500 },
+      { name: "Week-Long Course", price: 2000 },
+      { name: "Customized Training Program", price: 5000 },
+    ],
   },
   {
     title: "Drone Services",
     description:
       "Our professional drone services include aerial photography, videography, surveying, and inspection. We use state-of-the-art drones and technology to capture high-resolution images and videos, providing valuable insights for various industries such as real estate, construction, and agriculture.",
     icon: <FaPlane size={48} color={Assets.colors.primary} />,
-    button: require("../../drone/Drone"),
-    // prices: [
-    //   { name: "Aerial Photography Session", price: 500 },
-    //   { name: "Aerial Videography Session", price: 1000 },
-    //   { name: "Surveying and Inspection", price: 2000 },
-    // ],
+    prices: [
+      { name: "Aerial Photography Session", price: 500 },
+      { name: "Aerial Videography Session", price: 1000 },
+      { name: "Surveying and Inspection", price: 2000 },
+    ],
   },
   {
     title: "Equipment Set-up",
     description:
       "We provide expert equipment setup services, ensuring your technology infrastructure is installed correctly and efficiently. Our services include setting up computers, networks, servers, and other IT equipment, tailored to meet the specific needs of your business.",
     icon: <FaDesktop size={48} color={Assets.colors.primary} />,
-    button: require(""),
-    // prices: [
-    //   { name: "Basic Equipment Setup", price: 500 },
-    //   { name: "Advanced Equipment Setup", price: 2000 },
-    //   { name: "Full Office Setup", price: 5000 },
-    // ],
+    prices: [
+      { name: "Basic Equipment Setup", price: 500 },
+      { name: "Advanced Equipment Setup", price: 2000 },
+      { name: "Full Office Setup", price: 5000 },
+    ],
   },
 ];
 
@@ -146,11 +140,33 @@ const Services: React.FunctionComponent = () => {
                   mBottom={0}
                   mLeft={0}
                   mRight={0}
-                  bRadiusColor={getColor("light")}
-                  onClickButton={() => {
-                    if (service.button) {
-                      window.location.href = service.button;
-                    }
+                  bRadiusColor={getColor('light')}
+                  onClickButton={async () => {
+                    const convertedPrices = await Promise.all(
+                      service.prices.map(async (price) => {
+                        const convertedAmount = await convertToCurrency(
+                          price.price
+                        );
+                        return `${price.name}: ${convertedAmount}`;
+                      })
+                    );
+
+                    dispatch(
+                      updateModalContent({
+                        appTitle: service.title,
+                        appBody: `
+                          <div class="modal-content">
+                            <p>${service.description}</p>
+                            <ul style="list-style: none;">
+                              ${convertedPrices
+                                .map((price) => `<li>${price}</li>`)
+                                .join("")}
+                            </ul>
+                          </div>
+                        `,
+                      })
+                    );
+                    dispatch(updateModal(true));
                   }}
                 />
               }
@@ -164,44 +180,3 @@ const Services: React.FunctionComponent = () => {
 };
 
 export default Services;
-
-// onClickButton={() => {
-//   if (service.button) {
-//     window.location.href = service.button;
-//   }
-// }}
-//     onClickButton={async () => {
-//       const convertedPrices = await Promise.all(
-//         service.prices.map(async (price) => {
-//           const convertedAmount = await convertToCurrency(
-//             price.price
-//           );
-//           return `${price.name}: ${convertedAmount}`;
-//         })
-//       );
-
-//       dispatch(
-//         updateModalContent({
-//           appTitle: service.title,
-//           appBody: `
-//             <div class="modal-content">
-//               <p>${service.description}</p>
-//               <ul style="list-style: none;">
-//                 ${convertedPrices
-//                   .map((price) => `<li>${price}</li>`)
-//                   .join("")}
-//               </ul>
-//             </div>
-//           `,
-//         })
-//       );
-//       dispatch(updateModal(true));
-//     }}
-//   />
-// }
-//         />
-//       ))}
-//     </div>
-//     <BenefitsSection />
-//   </div>
-// </div>
