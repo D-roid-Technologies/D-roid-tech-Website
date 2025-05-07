27933cde7072348557ac439991c369ba15f595d6
stellanechi <stellanechi65@gmail.com>
2025-05-07 14:42:08 +0100

    updated toolbox

--- src/app/ui/pages/toolboxpage/ToolBoxPage.tsx
+++ src/app/ui/pages/toolboxpage/ToolBoxPage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import NavBar from "../../components/navbar/NavBar";
-import { FaCompressArrowsAlt, FaPalette } from "react-icons/fa";
+import { FaCompressArrowsAlt, FaPalette, FaImages } from "react-icons/fa";
+import { BiSolidCrop } from "react-icons/bi";
+import { CgColorPicker } from "react-icons/cg";
+import { FaFileWord } from "react-icons/fa6";
 import CoreValueCardTwo from "../../components/CoreValueCard/CoreValueCardTwo";
 
 const tools = [
@@ -9,69 +12,72 @@ const tools = [
     description:
       "Efficiently resize and optimize images for any device or platform. Maintain quality while reducing file size for faster loading.",
     icon: FaCompressArrowsAlt({ size: 24 }),
-    category: "Image Tools"
+    category: "Image Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Color Converter",
     description:
       "Transform images between color spaces (RGB, CMYK, HSL) with precise calibration. Perfect for print-ready files and digital displays.",
     icon: FaPalette({ size: 24 }),
-    category: "Color Tools"
+    category: "Color Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Image Compressor",
-    description:
-      "Reduce file size while maintaining quality",
-    icon: FaPalette({ size: 24 }),
-    category: "Image Tools"
+    description: "Reduce file size while maintaining quality",
+    icon: FaImages({ size: 24 }),
+    category: "Image Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Crop Tool",
-    description:
-      "Trim or cut images to focus on specific parts",
-    icon: FaPalette({ size: 24 }),
-    category: "Image Tools"
+    description: "Trim or cut images to focus on specific parts",
+    icon: BiSolidCrop({ size: 24 }),
+    category: "Image Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Color Picker",
     description:
       "Select and copy hex/RGB/HSV codes from a color palette or screen",
-    icon: FaPalette({ size: 24 }),
-    category: "Color Tools"
+    icon: CgColorPicker({ size: 24 }),
+    category: "Color Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Word Counter",
-    description:
-      "Count words, characters, and paragraphs",
-    icon: FaPalette({ size: 24 }),
+    description: "Count words, characters, and paragraphs",
+    icon: FaFileWord({ size: 24 }),
+    link: "/calculator/scientific",
   },
   {
     title: "Currency Converter",
-    description:
-      "Get real-time conversion rates for global currencies",
+    description: "Get real-time conversion rates for global currencies",
     icon: FaPalette({ size: 24 }),
-    category: "Calculation Tools"
+    category: "Calculation Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "JSON Formatter",
-    description:
-      "Beautify and validate JSON code",
+    description: "Beautify and validate JSON code",
     icon: FaPalette({ size: 24 }),
-    category: "Developer Tools"
+    category: "Developer Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "UUID Generator",
-    description:
-      "Generate unique identifiers for development",
+    description: "Generate unique identifiers for development",
     icon: FaPalette({ size: 24 }),
-    category: "Developer Tools"
+    category: "Developer Tools",
+    link: "/calculator/scientific",
   },
   {
     title: "Base64 Encoder/Decoder",
-    description:
-      "Encode or decode base64 strings",
+    description: "Encode or decode base64 strings",
     icon: FaPalette({ size: 24 }),
-    category: "Developer Tools"
+    category: "Developer Tools",
+    link: "/calculator/scientific",
   },
 ];
 
@@ -84,7 +90,9 @@ const ToolBoxPage: React.FunctionComponent = () => {
         <div className="software-main-content">
           <h1 className="software-header">D'roid ToolBox</h1>
           <p>
-            Toolbox is your ultimate Android companion — a powerful all-in-one utility app designed to help you manage, optimize, and customize your tasks with ease.
+            Toolbox is your ultimate Android companion — a powerful all-in-one
+            utility app designed to help you manage, optimize, and customize
+            your tasks with ease.
           </p>
         </div>
       </div>
@@ -103,6 +111,7 @@ const ToolBoxPage: React.FunctionComponent = () => {
               title={tech.title}
               description={tech.description}
               icon={tech.icon}
+              link={tech.link}
               className="process-card"
             />
           ))}
