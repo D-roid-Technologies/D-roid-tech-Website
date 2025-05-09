cede4208b8102fd2098db199c800fdd6bc6395f8
stellanechi <stellanechi65@gmail.com>
2025-05-09 11:28:53 +0100

    updated pages

--- src/app/ui/pages/toolboxpage/colorconverter/ColorConverter.tsx
+++ src/app/ui/pages/toolboxpage/colorconverter/ColorConverter.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import NavBar from "../../../components/navbar/NavBar";
+import ColorConv from "../../../components/toolboxfolder/Colorconv/ColorConv";
 
 const ColorConverter = () => {
-  return <div>ColorConverter</div>;
+  return (
+    <div>
+      <NavBar />
+      {/* Hero */}
+      <div className="software-main">
+        <div className="software-main-content">
+          <h1 className="software-header"> Droid Color Converter</h1>
+          <p>
+            is a sleek, intuitive tool that lets you instantly switch between
+            color formats like HEX, RGB, and HSL with pinpoint accuracy. Whether
+            you're a designer perfecting a palette or a developer fine-tuning UI
+            elements, Color Converter simplifies your workflow and ensures color
+            precision every time.
+          </p>
+        </div>
+      </div>
+      {/* Items */}
+
+      {/* color converter */}
+      <ColorConv />
+    </div>
+  );
 };
 
 export default ColorConverter;
