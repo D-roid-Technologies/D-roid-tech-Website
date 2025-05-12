ebaa00c64a79f921b25c56835f6066e985c0171a
developerekene <developerekene@gmail.com>
2025-05-12 21:43:12 +0100

    storing the signin and login feature --

--- src/app/ui/pages/schedule/SchedulePage.tsx
+++ src/app/ui/pages/schedule/SchedulePage.tsx
@@ -12,19 +12,16 @@ const SchedulePage: React.FunctionComponent = () => {
       <div className="software-main">
         <div className="wrapper">
           <div className="software-main-content">
-            <h1 className="software-header">Driod Schedules</h1>
+            <h1 className="software-header">D'roid Schedules</h1>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi a
-              id dolores, odit nesciunt recusandae nisi veritatis dolore. Modi
-              provident earum deserunt nostrum quibusdam, accusamus saepe. Illo
-              nostrum ea placeat!
+              D'roid Schedules is a smart scheduling tool that helps users organize, manage, and keep track of tasks, events, and activities with ease and efficiency.
             </p>
           </div>
         </div>
       </div>
-<section>
-  <TaskScheduler />
-</section>
+      <section>
+        <TaskScheduler />
+      </section>
 
     </div>
   );
