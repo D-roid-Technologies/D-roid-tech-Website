b80b87710e10ee58fc5eafdf9bb14addc9553dda
livinusonynso <livinusonyenso@gmail.com>
2025-06-18 20:47:13 +0100

    fix git conflict

--- src/app/redux/Store.tsx
+++ src/app/redux/Store.tsx
@@ -8,7 +8,6 @@ import { ContactSlice } from "./slices/ContactSlice";
 import { TestimonialSlice } from "./slices/TestimonialSlice";
 import { projectSlice } from "./slices/projectSlice";
 import { userSlice } from "./slices/User";
-<<<<<<< HEAD
 import { locationSlice } from "./slices/Location";
 import { allUsersSlice } from "./slices/AllUserSlice";
 import { signInAndOutSlice } from "./slices/SignInAndOutSlice";
@@ -17,15 +16,6 @@ import { affiliatedAppsSlice } from "./slices/affiliatedAppsSlice";
 import tasksReducer from "./slices/tasksSlice";
 import announcementsReducer from "./slices/Annoucements";
 import { trainingSlice } from './slices/TrainingsSlice';
-=======
-import { locationSlice } from './slices/Location';
-import { allUsersSlice } from './slices/AllUserSlice';
-import { signInAndOutSlice } from './slices/SignInAndOutSlice';
-import { payslipSlice } from './slices/paySlipSlice';
-import { affiliatedAppsSlice } from './slices/affiliatedAppsSlice';
-import { trainingSlice } from './slices/TrainingsSlice';
-import tasksReducer from "./slices/tasksSlice";
->>>>>>> f5a16d9cf0ac84dffac3148f1f280dc611a8befa
 
 const persistConfig = {
   key: 'root',
@@ -46,15 +36,9 @@ const rootReducer = combineReducers({
   SignInO: signInAndOutSlice.reducer,
   payslip: payslipSlice.reducer,
   connectedApps: affiliatedAppsSlice.reducer,
-<<<<<<< HEAD
   tasks: tasksReducer,
   announcements: announcementsReducer,
   trainings : trainingSlice.reducer
-=======
-  trainings : trainingSlice.reducer,
-  tasks: tasksReducer,
-
->>>>>>> f5a16d9cf0ac84dffac3148f1f280dc611a8befa
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
