65d16e7006e05af51fcb34867ef6ab6d58246606
livinusonynso <livinusonyenso@gmail.com>
2025-08-05 13:11:04 +0100

    WIP: current version before checking older StudentDashboard card design

---
+++ hboard.tsx
@@ -0,0 +1,17 @@
+[33mcommit 81a1b429301785f397c7e9e4455a961e70f688a1[m
+Author: livinusonynso <livinusonyenso@gmail.com>
+Date:   Mon Aug 4 17:17:13 2025 +0100
+
+    make all major tables responsive across screen sizes
+
+[33mcommit 86bb2450b572cde4d0cc2192eb3f8ccd129f1060[m
+Author: livinusonynso <livinusonyenso@gmail.com>
+Date:   Mon Jul 28 17:35:11 2025 +0100
+
+    Refactored All Students page styling for better visual consistency
+
+[33mcommit 84f77743a1addbe392b20e163b5ef475e41c54dd[m
+Author: livinusonynso <livinusonyenso@gmail.com>
+Date:   Fri Jul 25 16:58:01 2025 +0100
+
+    create All Students component with category structure (Creche, Nursery, Primary, Secondary)
